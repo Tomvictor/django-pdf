08d8c173e7d74a2efe9d07209782a2b42e893a63
Tom Victor <vjtomvictor@icloud.com>
2017-03-04 23:36:48 +0530

    More codes on view

--- sitemain/views.py
+++ sitemain/views.py
@@ -8,3 +8,20 @@ def home(request):
         "objects":BillBook.objects.all()
         }
     return render(request,'home.html',context)
+
+
+from django.http import HttpResponse
+from django.views.generic import View
+
+from yourproject.utils import render_to_pdf #created in step 4
+
+class GeneratePdf(View):
+    def get(self, request, *args, **kwargs):
+         data = {
+              'today': datetime.date.today(), 
+              'amount': 39.99,
+             'customer_name': 'Cooper Mann',
+             'order_id': 1233434,
+         }
+         pdf = render_to_pdf('invoice.html', data)
+         return HttpResponse(pdf, content_type='application/pdf')
